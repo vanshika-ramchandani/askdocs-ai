7296d50b53c107bc1b08d3b7bcc49ac47d6c1eef
Vanshika Ramchandani <vanshikaramchandani8@gmail.com>
2026-05-08 23:41:29 +0530

    jina api added

--- backend/embeddings/embedder.py
+++ backend/embeddings/embedder.py
@@ -1,12 +1,50 @@
-from sentence_transformers import SentenceTransformer
+import requests
+import numpy as np
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class EmbeddingGenerator:
 
     def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
 
-    def generate_embeddings(self, chunks):
+        self.api_key = os.getenv("JINA_API_KEY")
 
-        embeddings = self.model.encode(chunks)
+        self.url = "https://api.jina.ai/v1/embeddings"
 
-        return embeddings
+
+    def generate_embeddings(self, texts):
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+        embeddings = []
+
+        for text in texts:
+
+            payload = {
+                "model": "jina-embeddings-v2-base-en",
+                "input": [text]
+            }
+
+            response = requests.post(
+                self.url,
+                headers=headers,
+                json=payload
+            )
+
+            data = response.json()
+
+            embedding = data["data"][0]["embedding"]
+
+            embeddings.append(embedding)
+
+        return np.array(
+            embeddings,
+            dtype="float32"
+        )
